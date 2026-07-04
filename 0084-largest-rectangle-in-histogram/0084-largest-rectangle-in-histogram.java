8e9f2c06c81e2096c1fc39465cffbc12d1a37d22
Deepusingh899 <133624601+Deepusingh899@users.noreply.github.com>
2026-07-04 20:45:18 +0530

    Time: 116 ms (5.79%), Space: 76.2 MB (92.32%) - LeetHub

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -0,0 +1,48 @@
+class Solution {
+     public static int[] nextSmaller(int arr[]){
+        int[] nextSmaller=new int[arr.length];
+        Stack<Integer> s=new Stack<>();
+        for(int i=arr.length-1;i>=0;i--){
+            int curr=arr[i];
+            while(!s.isEmpty() && arr[s.peek()]>=curr){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                nextSmaller[i]=arr.length;
+            }else{
+                nextSmaller[i]=s.peek();
+            }
+            s.push(i);
+        }
+        return nextSmaller;
+    }
+    public static int[] prevSmaller(int arr[]){
+        int[] prevSmaller=new int[arr.length];
+        Stack<Integer> s=new Stack<>();
+        for(int i=0;i<arr.length;i++){
+            int curr=arr[i];
+            while(!s.isEmpty() && arr[s.peek()]>=curr){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                prevSmaller[i]=-1;
+            }else{
+                prevSmaller[i]=s.peek();
+            }
+            s.push(i);
+        }
+        return prevSmaller;
+    }
+    public int largestRectangleArea(int[] heights) {
+        int maxArea=0;
+        int[] nextSmaller=nextSmaller(heights);
+        int[] prevSmaller=prevSmaller(heights);
+        for(int i=0;i<heights.length;i++){
+            int height=heights[i];
+            int widht=nextSmaller[i]-prevSmaller[i]-1;
+            int area=height*widht;
+            maxArea=Math.max(maxArea,area);
+        }
+        return maxArea;
+    }
+}
