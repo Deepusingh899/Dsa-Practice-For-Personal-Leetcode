627c26b97f4a775d40c6cdc474ae5add3e05f889
Deepusingh899 <133624601+Deepusingh899@users.noreply.github.com>
2026-08-15 17:50:43 +0530

    Time: 0 ms (100.00%) | Memory: 46 MB (94.00%) - LeetSync

--- Leetcode/1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.java
+++ Leetcode/1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.java
@@ -19,6 +19,6 @@ class Solution {
         root.left=removeLeafNodes(root.left,target);
         root.right=removeLeafNodes(root.right,target);
         if(root.left==null && root.right==null && root.val==target) return null;
-        else return root;        
+        return root;        
     }
 }
