c135ea5e2915e574dc060178186142576891e178
Deepusingh899 <133624601+Deepusingh899@users.noreply.github.com>
2026-08-16 14:49:29 +0530

    Time: 1 ms (99.68%) | Memory: 44.8 MB (94.19%) - LeetSync

--- Leetcode/662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
+++ Leetcode/662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
@@ -23,19 +23,16 @@ class Solution {
         }
     }
     public int widthOfBinaryTree(TreeNode root) {
-        Queue<Pair> q=new LinkedList<>();
-        q.add(new Pair(root,0));
+        Deque<Pair> q=new ArrayDeque<>();
+        q.addLast(new Pair(root,0));
         int max=Integer.MIN_VALUE;
         while(!q.isEmpty()){
             int size=q.size();
-            int start=0,end=0;
+            int start=q.getFirst().idx,end=q.getLast().idx;
             for(int i=0;i<size;i++){
-                Pair p=q.remove();
-                int index=p.idx;
-                if(i==0) start=index;
-                if(i==size-1) end=index;
-                if(p.root.left!=null) q.add(new Pair(p.root.left,2*index+1));
-                if(p.root.right!=null) q.add(new Pair(p.root.right,2*index+2));
+                Pair p=q.removeFirst();
+                if(p.root.left!=null) q.addLast(new Pair(p.root.left,2*p.idx+1));
+                if(p.root.right!=null) q.addLast(new Pair(p.root.right,2*p.idx+2));
             }
             max=Math.max(max,end-start+1);
         }
