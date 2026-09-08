592438131af20190a873d087500d25d510e49849
Deepusingh899 <133624601+Deepusingh899@users.noreply.github.com>
2026-09-08 21:02:17 +0530

    Time: 1 ms (26.04%) | Memory: 44.1 MB (9.00%) - LeetSync

---
+++ Leetcode/88-merge-sorted-array/merge-sorted-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for(int i=0;i<m;i++){
+            minHeap.add(nums1[i]);
+        }
+        for(int i=0;i<n;i++){
+            minHeap.add(nums2[i]);
+        }
+        int i=0;
+        while(!minHeap.isEmpty()){
+            nums1[i++]=minHeap.remove();
+        }
+        // return nums1;
+    }
+}
