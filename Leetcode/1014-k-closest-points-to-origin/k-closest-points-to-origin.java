c1fe735cc65dafe60ad00a6ff0257491db3b27ae
Deepusingh899 <133624601+Deepusingh899@users.noreply.github.com>
2026-09-14 12:55:24 +0530

    Time: 31 ms (50.32%) | Memory: 51.3 MB (98.38%) - LeetSync

--- Leetcode/1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ Leetcode/1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,15 +1,30 @@
 class Solution {
+    public class Closest implements Comparable<Closest> {
+        int [] arr;
+        int dist;
+        Closest(int arr[],int dist){
+            this.arr=arr;
+            this.dist=dist;
+        }
+        @Override
+        public int compareTo(Closest cl){
+            return cl.dist-this.dist;
+        }
+    }
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b) -> dist(b) - dist(a));
+        PriorityQueue<Closest> pq=new PriorityQueue<>();
         for(int [] num:points){
-            pq.add(num);
+            int a=num[0];
+            int b=num[1];
+            int dist=a*a+b*b;
+            pq.add(new Closest(num,dist));
             if(pq.size()>k){
                 pq.poll();
             }
         }
         int [][]nums=new int[k][];
         for(int i=0;i<k;i++){
-            nums[i]=pq.poll();
+            nums[i]=pq.poll().arr;
         }
         return nums;
     }
